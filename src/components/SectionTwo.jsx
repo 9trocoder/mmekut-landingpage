b5bada82da209a65314c6c0ab4af723cf1c32024
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2023-02-14 13:51:02 +0400

    update

---
+++ src/components/SectionTwo.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./components.css"
+import reminderimg from "../assets/svg/"
+
+function SectionTwo() {
+  return (
+    <div className="sectionbody">
+        <div className="sectioncnt">
+            <div className="sectiontwoleft">
+                <div className="sectag">
+                    <label>Reminder</label>
+                </div>
+                <h2>Get reminded anytime, any where</h2>
+                <p>With mmekut notification, you can get reminders, communications and other timely information anytime, and anywhere.</p>
+            </div>
+            <div className="sectiontworight"></div>
+        </div>
+    </div>
+  )
+}
+
+export default SectionTwo
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,6 +3,7 @@ import Hero from "../components/Hero";
 import Join from "../components/Join";
 import Navbar from "../components/Navbar";
 import SectionOne from "../components/SectionOne";
+import SectionTwo from "../components/SectionTwo";
 
 function Homepage() {
   return (
@@ -11,6 +12,7 @@ function Homepage() {
       <Hero />
       <Join />
       <SectionOne />
+      <SectionTwo />
     </>
   );
 }
