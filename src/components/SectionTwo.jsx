d5ec11a56b4079351d51250c1998b5a0ab9f41ca
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2023-02-14 15:11:41 +0400

    Update SectionTwo.jsx

--- src/components/SectionTwo.jsx
+++ src/components/SectionTwo.jsx
@@ -13,7 +13,7 @@ function SectionTwo() {
                 <h2>Get reminded anytime, any where</h2>
                 <p>With mmekut notification, you can get reminders, communications and other timely information anytime, and anywhere.</p>
             </div>
-            <div className="sectiontworight"></div>
+           <img src={reminderimg} alt="" />
         </div>
     </div>
   )
