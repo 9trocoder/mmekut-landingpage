00c4701d17a32104deebd1bc57acf418104983c9
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2023-03-11 16:17:32 +0400

    update

--- src/components/Testimonies.jsx
+++ src/components/Testimonies.jsx
@@ -5,13 +5,13 @@ function Testimonies() {
     <div className="tstcnt">
         <div className="testimonybody">
         <div className="testimonyright">
-            <div className="tstrleft"></div>
+            
             <div className="tstrright">
                 <div className="tsttag">
                     <h6>Testimonial</h6>
                 </div>
                 <h2>What people say about us.</h2>
-            </div>
+            </div><div className="tstrleft"></div>
         </div>
     </div>
     </div>
