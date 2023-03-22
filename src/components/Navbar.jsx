863404bc4b9ccabea2b565337cd0f53072950b82
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2023-03-22 22:02:35 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import navlogo from "../assets/svg/mmekutlogo.svg";
 import menu from "../assets/svg/menu.svg";
